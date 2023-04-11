7b7ab72aaa06a69be3b27741ef1647b99a51b9f0
javascriptsucks <evix1988@live.cn>
2023-04-11 01:19:42 -0600

    usesubscription apollo hook

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,5 +1,6 @@
-import { gql } from "@apollo/client";
-import { useMutation } from "@apollo/client";
+import { useMutation, gql } from "@apollo/client";
+import { Button } from "antd";
+import { useState } from "react";
 
 const CREATE_USER = gql`
 	mutation CreateUser($name: String!, $email: String!) {
@@ -14,6 +15,8 @@ const CREATE_USER = gql`
 	}
 `;
 function CreateUser() {
+	const [createUser, { data, loading, error }] = useMutation(CREATE_USER);
+
 	const [state, setState] = useState({
 		name: "",
 		email: "",
@@ -22,7 +25,38 @@ function CreateUser() {
 		e.preventDefault();
 		createUser({ variables: { name: state.name, email: state.email } });
 	};
-	return <div></div>;
+	console.log(data);
+	return (
+		<div className='lg:fixed bottom-0 left-0 w-full border-t border-gray-300'>
+			<form
+				className='lg:px-8 pt-2 pb-2'
+				onSubmit={handleSubmit}>
+				<div className='lg:flex flex-wrap flex-between items-center justify-center lg:p-0 p-6'>
+					<h4 className='font-bold lg:pr-4 mb-2'>Create New User</h4>
+					<div className='lg:pr-4 mb-2'>
+						<input
+							type='text'
+							value={state.name}
+							onChange={(e) => setState({ ...state, name: e.target.value })}
+						/>
+					</div>
+					<div className='lg:pr-4 mb-2'>
+						<input
+							type='text'
+							value={state.email}
+							onChange={(e) => setState({ ...state, email: e.target.value })}
+						/>
+					</div>
+				</div>
+
+				<Button
+					type='primary'
+					htmlType='submit'>
+					Create User
+				</Button>
+			</form>
+		</div>
+	);
 }
 
 export default CreateUser;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,6 @@
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useSubscription } from "@apollo/client";
 import UserAvatar from "./UserAvatar";
-
-// import CreateUser from "./CreateUser";
+import CreateUser from "./CreateUser";
 
 export default function Users({ selectUser }) {
 	const GET_USERS = gql`
@@ -14,13 +13,43 @@ export default function Users({ selectUser }) {
 			}
 		}
 	`;
-	const { loading, error, data } = useQuery(GET_USERS);
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error :( Because: {error.message}</p>;
-	console.log(data.users);
+	const NEW_USER_SUBSCRIPTION = gql`
+		subscription onUser {
+			user {
+				id
+				name
+				email
+				postsCount
+			}
+		}
+	`;
+
+	const {
+		loading: queryLoading,
+		error: queryError,
+		data: queryData,
+	} = useQuery(GET_USERS);
+
+	const { data: subData, error: subError } = useSubscription(
+		NEW_USER_SUBSCRIPTION,
+		{
+			onData: ({ subscriptionData }) => {
+				console.log(subscriptionData);
+			},
+		}
+	);
+
+	if (queryLoading) return <p>Loading...</p>;
+	if (queryError) return <p>Error :( Because: {error.message}</p>;
+
+	console.log(subData);
+
 	return (
 		<div className='container flex max-w-2xl min-w-fit flex-row flex-wrap intems-center pb-16'>
-			{data.users.map((user) => (
+			<div className='lg:m-4 lg:w-1/4 w-full rounded shadow-lg'>
+				<CreateUser />
+			</div>
+			{queryData?.users?.map((user) => (
 				<div
 					key={user.id}
 					className='lg:w-1/3 p-4 text-center inline hover:scale-105  bg-gray-400 rounded-full'
@@ -28,6 +57,14 @@ export default function Users({ selectUser }) {
 					<UserAvatar user={user} />
 				</div>
 			))}
+			{subData?.user && (
+				<div
+					key={subData.user.id}
+					className='lg:w-1/3 p-4 text-center inline hover:scale-105  bg-gray-400 rounded-full'
+					onClick={() => selectUser(subData.user)}>
+					<UserAvatar user={subData.user} />
+				</div>
+			)}
 		</div>
 	);
 }
